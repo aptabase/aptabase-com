fd7b210797c98a0984b24fe9e9709438724bab81
goenning <me@goenning.net>
2023-03-29 19:01:36 +0100

    add astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
   server: { port: 4000 },
 });
