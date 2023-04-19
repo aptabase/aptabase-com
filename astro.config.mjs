36c8145faa6d7d7c77dd13fa7797fd1925a61c2b
goenning <me@goenning.net>
2023-04-19 10:53:01 +0100

    add pricing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://aptabase.com",
   trailingSlash: "never",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), alpine()],
   server: {
     port: 4000,
   },
