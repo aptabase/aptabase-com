34d93428a40b0682fd671bf6698b91d7b735e77a
goenning <me@goenning.net>
2023-06-21 20:15:18 +0100

    temporary better version

---
+++ src/lib/frameworks.ts
@@ -0,0 +1,52 @@
+export default [
+  {
+    name: "Native iOS (Swift)",
+    image: "/tools/apple.svg",
+    href: "/for-swift",
+  },
+  {
+    name: "Native Android (Kotlin)",
+    image: "/tools/android.svg",
+    href: "/for-android",
+  },
+  {
+    name: "Flutter",
+    image: "/tools/flutter.svg",
+    href: "/for-flutter",
+  },
+  {
+    name: "Electron",
+    image: "/tools/electron.svg",
+    href: "/for-electron",
+  },
+  {
+    name: "NativeScript",
+    image: "/tools/nativescript.svg",
+    href: "/for-nativescript",
+  },
+  {
+    name: "Tauri",
+    image: "/tools/tauri.svg",
+    href: "/for-tauri",
+  },
+  {
+    name: "React Native",
+    image: "/tools/react-native.svg",
+    href: "/for-react-native",
+  },
+  {
+    name: "Web App",
+    image: "/tools/javascript.svg",
+    href: "/for-webapps",
+  },
+  {
+    name: ".NET MAUI",
+    image: "/tools/dotnet.svg",
+    href: "/for-maui",
+  },
+  {
+    name: "Unreal Engine",
+    image: "/tools/unreal.svg",
+    href: "/for-unreal",
+  },
+];
