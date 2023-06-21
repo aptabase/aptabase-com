031c73658901ed7754eb1d8a89a9fbd74b8ef6cf
goenning <me@goenning.net>
2023-06-21 15:28:20 +0100

    better navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
@@ -16,6 +16,6 @@ export default defineConfig({
     assets: true,
   },
   image: {
-    service: "astro/assets/services/sharp",
+    service: sharpImageService(),
   },
 });
